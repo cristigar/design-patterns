4686476741bd514453b919b26d019209cc90b297
Cristian Garabagiu <cristian.garabagiu@gmail.com>
2019-09-25 09:43:03 +0300

    Added phpstan

--- src/Creational/Builder/CompleteBurgerBuilder.php
+++ src/Creational/Builder/CompleteBurgerBuilder.php
@@ -8,6 +8,8 @@ interface CompleteBurgerBuilder
 {
     public function getSize(): int;
 
+    public function hasPepperoni(): bool;
+
     public function hasLettuce(): bool;
 
     public function hasCheese(): bool;
